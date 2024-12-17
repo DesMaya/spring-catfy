1d653ca4db4d469b4dd0b66c723f8d856d45f2e4
Maya <mayaflorentinodasilva@gmail.com>
2024-12-17 18:32:59 -0300

    Criada a classe principal com seu menu e as respectivas funções a serem implementadas futuramente

--- src/main/java/maya/catfy/CatfyApplication.java
+++ src/main/java/maya/catfy/CatfyApplication.java
@@ -1,13 +1,20 @@
 package maya.catfy;
 
+import maya.catfy.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatfyApplication {
+public class CatfyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatfyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
 }
