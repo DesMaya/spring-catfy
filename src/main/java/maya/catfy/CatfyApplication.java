9167d53fb3b779ada6583de21da1e55838f6704a
Maya <mayaflorentinodasilva@gmail.com>
2024-12-17 18:28:17 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/maya/catfy/CatfyApplication.java
@@ -0,0 +1,13 @@
+package maya.catfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maya/catfy/CatfyApplicationTests.java
@@ -0,0 +1,13 @@
+package maya.catfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
