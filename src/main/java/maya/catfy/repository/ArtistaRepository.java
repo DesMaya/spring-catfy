beeced781541fe75ed42b894817d12ace6b336fd
Maya <mayaflorentinodasilva@gmail.com>
2024-12-17 19:52:01 -0300

    Criado método para encontrar os artistas pelo nome

--- src/main/java/maya/catfy/repository/ArtistaRepository.java
+++ src/main/java/maya/catfy/repository/ArtistaRepository.java
@@ -3,5 +3,8 @@ package maya.catfy.repository;
 import maya.catfy.model.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+    Optional<Artista> findByNomeContainingIgnoreCase(String nome);
 }
